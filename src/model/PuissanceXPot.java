31d25fd5f080bd2cefe218f9c68b0097c1870a18
Constant <constantsuchet@gmail.com>
2025-05-06 08:59:36 +0200

    Complétation de la classe gérant le pot des joueurs

--- src/model/PuissanceXPot.java
+++ src/model/PuissanceXPot.java
@@ -5,17 +5,35 @@ import boardifier.model.ContainerElement;
 
 public class PuissanceXPot extends ContainerElement {
     
-    // TODO: Attributs
-    // 1. Identifiant du joueur propriétaire du pot
+    // Identifiant du joueur propriétaire du pot
+    private int playerId;
+    private int nbDiscs;
     
-    public PuissanceXPot(int x, int y, int nbDiscs, GameStageModel gameStageModel) {
+    public PuissanceXPot(int x, int y, int nbDiscs, GameStageModel gameStageModel, int playerId) {
         super("discpot", x, y, 1, nbDiscs, gameStageModel);
         
-        // TODO: Initialisation
-        // 1. Stocker l'identifiant du joueur
+        this.playerId = playerId;
+        this.nbDiscs = nbDiscs;
     }
-    
-    // TODO: Méthodes utilitaires
-    // 1. Obtenir le prochain jeton disponible
-    // 2. Vérifier s'il reste des jetons
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public int getNbDiscs() {
+        return nbDiscs;
+    }
+
+    public boolean isDiscAvailable() {
+        return !this.isEmpty();
+    }
+
+    public Disc getNextDisc() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        Disc newDisc = new Disc(0, 0, playerId, this.gameStageModel);
+        this.removeElement(newDisc);
+        return newDisc;
+    }
 }
