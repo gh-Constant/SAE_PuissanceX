70f3f32cb524c675aed84fde13bf617ac96c1439
Constant <constantsuchet@gmail.com>
2025-05-06 09:12:37 +0200

    Complétation du StageFactory et fixes dans PuissanceXPot

--- src/model/PuissanceXPot.java
+++ src/model/PuissanceXPot.java
@@ -8,6 +8,7 @@ public class PuissanceXPot extends ContainerElement {
     // Identifiant du joueur propriétaire du pot
     private int playerId;
     private int nbDiscs;
+    private Disc[] discs;
     
     public PuissanceXPot(int x, int y, int nbDiscs, GameStageModel gameStageModel, int playerId) {
         super("discpot", x, y, 1, nbDiscs, gameStageModel);
@@ -20,20 +21,31 @@ public class PuissanceXPot extends ContainerElement {
         return playerId;
     }
 
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public void setDiscs(Disc[] discs) {
+        this.discs = discs;
+    }
+
+    public Disc[] getDiscs() {
+        return discs;
+    }
+
+    public void addDisc(Disc disc) {
+        this.addElement(disc, 0, this.discs.length);
+    }
+
+    public void removeDisc(Disc disc) {
+        this.removeElement(disc);
+    }
+
     public int getNbDiscs() {
         return nbDiscs;
     }
 
     public boolean isDiscAvailable() {
         return !this.isEmpty();
-    }
-
-    public Disc getNextDisc() {
-        if (this.isEmpty()) {
-            return null;
-        }
-        Disc newDisc = new Disc(0, 0, playerId, this.gameStageModel);
-        this.removeElement(newDisc);
-        return newDisc;
     }
 }
